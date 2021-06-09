a210a79f4ac9c626e195ed94a16194e0fcfcc725
Saloni <salonimehta2127@gmail.com>
2021-06-08 20:10:48 -0400

    comments working properly

--- src/index.js
+++ src/index.js
@@ -3,43 +3,28 @@ document.addEventListener("DOMContentLoaded",()=>{
 // created individual fetch requests to fetch only certain brands
 const baseURL="https://makeup-api.herokuapp.com/api/v1/products.json?brand=";
 // multiple fetch requests for fetching individual brands 
-function fetchNyx()
-{
-    fetch(`${baseURL}nyx`)
-    .then(resp=>resp.json()).then(product=>displayData(product))
+const brands=["nyx","maybelline","clinique","milani"];
+function fetchAll()
+{   
+    brands.map((brands)=>{
+        fetch(`${baseURL}${brands}`)
+        .then(resp=>resp.json()).then(product=>displayData(product))
+    })
 }
-fetchNyx()
-function fetchMaybelline()
-{
-    fetch(`${baseURL}maybelline`)
-    .then(resp=>resp.json()).then(product=>displayData(product))
-}
-fetchMaybelline()
-function fetchClinique()
-{
-    fetch(`${baseURL}clinique`)
-    .then(resp=>resp.json()).then(product=>displayData(product))
-}
-fetchClinique()
-function fetchMilani()
-{
-    fetch(`${baseURL}milani`)
-    .then(resp=>resp.json()).then(product=>displayData(product))
-}
-fetchMilani()
-
+fetchAll();
 function displayData(product)
 {
-    product.forEach(element => {
+    product.map(element => {
         createCards(element)
     });
 }
-
 const mainContainer=document.getElementById("main-container");
 
-function createCards(product){
+function createCards(product)
+{
     const createDiv=document.createElement("div");
     createDiv.className="card"
+
     const aTag=document.createElement("a");
     aTag.href="#"
     aTag.className="link"
@@ -50,61 +35,60 @@ function createCards(product){
     img.src=product.image_link;
     img.className="product-image"
 
-        aTag.addEventListener("click",()=>
-        {  // **** all of these work ****
-            //  add to your blog post different ways to empty the innerHTML
-            // while (mainContainer.firstChild) mainContainer.removeChild(mainContainer.firstChild);
-            // mainContainer.querySelectorAll("*").forEach(el => el.remove())
-            mainContainer.innerHTML="";
+    aTag.addEventListener("click",()=>
+    {  // **** all of these work ****
+        //  add to your blog post different ways to empty the innerHTML
+        // while (mainContainer.firstChild) mainContainer.removeChild(mainContainer.firstChild);
+        // mainContainer.querySelectorAll("*").forEach(el => el.remove())
+        mainContainer.innerHTML="";
 
-            const name=document.createElement("h2")
-            name.innerText=product.name;
-            name.className="product-name"
+        const name=document.createElement("h2")
+        name.innerText=product.name;
+        name.className="product-name"
 
-            const img=document.createElement("img");
-            img.src=product.image_link;
-            img.className="product-image"
+        const img=document.createElement("img");
+        img.src=product.image_link;
+        img.className="product-image"
 
-            const p=document.createElement('p');
-            p.innerHTML=`Product description: <br> ${product.description}`;
+        const p=document.createElement('p');
+        p.innerHTML=`Product description: <br> ${product.description}`;
+
+        const productType=document.createElement("p")
+        productType.innerText=`Product Type: ${product.product_type.toUpperCase()}`;
             /// creates a form for reviews 
-                const commentForm=document.createElement("form")
-                commentForm.id="commentInput";
+        const commentForm=document.createElement("form")
+        commentForm.id="commentInput";
 
-                const commentInput=document.createElement("input")
-                commentInput.id="enter-comment"
-                commentInput.type="text";
-                commentInput.placeholder="Enter your Review";
-                
-                const submitReview=document.createElement("button");
-                submitReview.innerText="Submit Review";
-
-                commentForm.append(commentInput,submitReview);
-                commentForm.addEventListener("submit",(e)=>
-                    { 
-                    e.preventDefault();
-                    renderComments(commentInput.value)
-                    // const mainContainer=document.getElementById("main-container");
-                   // mainContainer.append(commentSection);
-                        fetch(`http://localhost:3000/reviews/`,{
-                        method:"POST",
-                        headers:{
-                        "Content-Type":"application/json",
-                        "accept":"application/json"
-                        },
-                        body:JSON.stringify({
-                        "review":commentInput.value,
-                        "productId":product.id
-                        })
-                        }).then(resp=>resp.json())
-                        e.target.reset();
-                        
-                    })
-                    fetchComments();
+        const commentInput=document.createElement("input")
+        commentInput.id="enter-comment"
+        commentInput.type="text";
+        commentInput.placeholder="Enter your Review";
+            
+        const submitReview=document.createElement("button");
+        submitReview.innerText="Submit Review";
+        commentForm.append(commentInput,submitReview);
+        commentForm.addEventListener("submit",(e)=>
+            { 
+            e.preventDefault();
+            renderComments(commentInput.value);
+            fetch(`http://localhost:3000/reviews/`,{
+            method:"POST",
+            headers:{
+            "Content-Type":"application/json",
+            "accept":"application/json"
+            },
+            body:JSON.stringify({
+            "review":commentInput.value,
+            "productId":product.id
+            })
+            }).then(resp=>resp.json())
+            e.target.reset();    
+            })
+            fetchComments(product.id);
                     // reviews.map(x=>x.productId===product.id)
                     
-            mainContainer.append(name,img,p,commentForm);
-        }) 
+        mainContainer.append(name,img,p,productType,commentForm);
+    }) 
      /// aTag ends here with the link listener
      ///************************************ 
      
@@ -133,44 +117,75 @@ function createCards(product){
         })
         }).then(resp=>resp.json())
     })
-    const h3=document.createElement("h3")
-    h3.innerText=`How many users liked: ${product.rating}`;
-    createDiv.append(aTag,img,p,addToCart,h3);
+    createDiv.append(aTag,img,p,addToCart);
     mainContainer.append(createDiv);
   
 }
-function renderComments(comment)
+function renderComments(comment) // // commentInput.value passed
 { 
-    
     const commentSection=document.createElement("li");
     commentSection.className="comment-box"
     const comUL=document.createElement("ul");
-    // const comInput=document.getElementById("enter-comment")
-    commentSection.innerHTML=comment;
+    commentSection.innerHTML=`📝${comment}`;
     comUL.append(commentSection);
     mainContainer.append(comUL);
 }
-function fetchComments()
-{   
-    // const review=e.target.children[0].value;
+
+function fetchComments(product)// product.id is passed to compare
+{  
     fetch(`http://localhost:3000/reviews/`).then(resp=>resp.json()).then(review=>review.map((x)=>{
-        //x.productId===product.id
-   renderComments(x.review)
-       } 
-    ))
-    
+        if(x.productId===product){
+        renderComments(x.review)}
+    }))
 }
-function renderCart(cart){
+
+function cart()
+{   
+    mainContainer.innerHTML="";
+    fetch(`http://localhost:3000/cart`).then(resp=>resp.json()).then(cart=>cart.map(cart=>renderCart(cart)))
+
+}
+
+document.getElementById("cart").addEventListener("click",cart)
+function deleteFromCart(ids) // passing card.id from delete button event listener
+{
+    //console.log(cart);
+    fetch(`http://localhost:3000/cart/${ids}`,{
+    method:"DELETE",
+    headers:{
+        "content-type":'application/json'
+    }
+    }).then(resp=>resp.json())
+}
+function renderCart(cart)
+{
     const h2=document.createElement("h2");
     h2.innerText=cart.title;
+
     const img= document.createElement("img");
     img.src=cart.img;
+    img.className="cartImage"
+
     const p= document.createElement("p");
     p.innerText=`$ ${cart.price}`
+    
+    const deleteButton=document.createElement("button");
+    deleteButton.innerText="delete item from cart"
+    deleteButton.className="deleteItem"
+
+    deleteButton.addEventListener("click",()=>
+    {   
+        deleteFromCart(cart.id);
+        mainContainer.innerHTML="";
+        fetch(`http://localhost:3000/cart`).then(resp=>resp.json()).then(cart=>cart.map(cart=>renderCart(cart)))
+    })
+
     const checkout=document.createElement("button");
     checkout.innerText="Place order for this Item";
+    checkout.className="placeOrder"
 
     checkout.addEventListener("click",()=>{
+
         mainContainer.innerHTML="";
         const order=document.createElement("h3");
         order.innerText="YAYYY!! Your order has been placed!!"
@@ -178,33 +193,25 @@ function renderCart(cart){
         h2.innerText="Thank you for shopping with us!"
         mainContainer.append(order,h2);
         const ids=cart.id;
-        console.log(cart);
-        fetch(`http://localhost:3000/cart/${ids}`,{
-        method:"DELETE",
-        headers:{
-            "content-type":'application/json'
-        }
-        }).then(resp=>resp.json())
+        deleteFromCart(ids); 
     })
-    mainContainer.append(h2,img,p,checkout);
+    mainContainer.append(h2,img,p,deleteButton,checkout);
 }
 
 /// cart button event
-document.getElementById("cart").addEventListener("click",cart)
 
-function cart()
-{   mainContainer.innerHTML="";
-    fetch(`http://localhost:3000/cart`).then(resp=>resp.json()).then(cart=>cart.forEach(cart=>renderCart(cart)))
-}
 // this function is solely for search button, in this case our search button is an submit button.
 // so a user can either click the button or just hit enter 
+
 function search()
 {
     // event listener on the form itself
     document.getElementById("searchbar").addEventListener("submit",(e)=>
     {
         e.preventDefault();
-        const val=e.target.lastElementChild.value.toString();
+        console.log(brands);
+        const val=e.target.lastElementChild.value.toString(); // value of this is input text in search field 
+
         // console.log(val);
         if(val.toLowerCase()==="nyx")
         {   
@@ -234,88 +241,25 @@ function search()
     })
 }
 search();
+
+function fetchNyx()
+{
+    fetch(`${baseURL}nyx`)
+    .then(resp=>resp.json()).then(product=>displayData(product))
+}
+function fetchMaybelline()
+{
+    fetch(`${baseURL}maybelline`)
+    .then(resp=>resp.json()).then(product=>displayData(product))
+}
+function fetchClinique()
+{
+    fetch(`${baseURL}clinique`)
+    .then(resp=>resp.json()).then(product=>displayData(product))
+}
+function fetchMilani()
+{
+    fetch(`${baseURL}milani`)
+    .then(resp=>resp.json()).then(product=>displayData(product))
+}
 })
-// function addToCart()
-// {
- 
-// }
-// function getComments()
-// {
-//     fetch("http://localhost:3000/reviews").then(resp=>resp.json())
-// }
-//  const btn=document.createElement("button")
-        //  btn.innerText="⬅️"
-        //  btn.className="arrow";
-        //  btn.addEventListener("click",()=>
-        //  { 
-        //      mainContainer.innerHTML="";
-        //     fetchNyx()
-        //     fetchMaybelline()
-        //     fetchClinique()
-        //     fetchMilani()
-        //  })
-
-
-// const searchText=document.getElementById("searchId");
-// const btn=document.getElementById("btnClick");
-// btn.addEventListener("click",()=>
-// {
-//     console.log(searchText.value);
-// })
-// searchText.addEventListener("keyup",(e)=>{
-//     if(e.keyCode===13)
-//     {
-//         e.preventDefault();
-//         btn.addEventListener("click",(e)=>
-//         {
-//             alert("i am workign");
-//         })
-//     }
-// })
-// function buttonClick(){
-//     alert("gtg");
-// }
- // Promise.all([nyx,maybelline,clinique].map(brand => 
-        //     fetch(`https://makeup-api.herokuapp.com/api/v1/products.json?${brand=}`)
-        //     .then(resp => resp.json())
-        //   )).then(product=>displayData(product));
-
-
-        // searchText.addEventListener("keyup",(e)=>{
-//     e.preventDefault();
-//    if(e.KeyCode===13)
-//    {
-//        btn.click();
-//    }
-// })
-// function click(){
-
-// }
-// header.append(mainContainer)
-  //  const ul=document.createElement("ul");
-        //  const tags=document.createElement("li");
-        //  tags.innerText=product.tag_list;
-        //  ul.append(tags);
-        // product[0].product_colors[0].hex_value);
-
-////////////tried in one request failed 
-
-        // function fetchRequest()
-// {
-//     const arr=["nyx","maybelline","clinique","milani"];
-// Promise.all(arr.map( ()=> 
-//             fetch(`https://makeup-api.herokuapp.com/api/v1/products.json?brand=${arr}`)
-//             .then(resp => resp.json())
-//           )).then(product=>displayData(product));
-
-
-
-// //     const arr=["nyx","maybelline","clinique","milani"];
-// //   arr.forEach(()=>{
-// //       fetch(`${baseURL}${arr}`).then(resp=>resp.json()).then(product=>displayData(product));
-// //   })
-// }
-// fetchRequest();
-//  let htmlstring=mainContainer.innerHTML;
-        // //  htmlstring= (htmlstring.trim) ? htmlstring.trim() : htmlstring.replace(/^\s+/,'');
-        //  htmlstring.innerText=" ";
